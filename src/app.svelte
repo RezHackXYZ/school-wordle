0479517766a43e9a4a325483d1dabd79064c10a0
RezHackXYZ <rhythmupa@gmail.com>
2025-04-12 16:37:20 +0530

    gfdgfd

--- src/app.svelte
+++ src/app.svelte
@@ -1,1 +1,34 @@
-Yo!
+<script>
+	import Game from "./game.svelte";
+</script>
+
+<div id="root">
+	<h1>CLASSROOM WORDLE <span>~made by Rhythm Upadhyay of 7th A</span></h1>
+	<Game />
+</div>
+
+<style>
+	:root {
+		background-color: #121212;
+		color: white;
+	}
+
+	#root {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		margin: 0;
+	}
+
+	h1 {
+		text-align: center;
+		margin: 5px 0px;
+		text-decoration: underline #444;
+	}
+
+	span {
+		font-size: 0.5em;
+		text-decoration: none;
+		color: #444;
+	}
+</style>
---
+++ src/game.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Keyboard from "./keyboard.svelte";
+    import Display from "./display.svelte";
+</script>
+
+<div id="root">
+    <Display />
+    <Keyboard />
+</div>
+
+<style>
+    #root {
+        width: calc(50% - 40px);
+        height: calc(100% - 40px);
+        border-radius: 20px;
+        margin: 20px;
+        margin-top: 5px;
+        border: 2px solid #444;
+    }
+</style>
